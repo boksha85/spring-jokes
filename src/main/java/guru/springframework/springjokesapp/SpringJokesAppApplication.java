52f41cad8581f9938647f79bd1c85d67b21d3804
Bojan Sutulovic <bsutulovic@novomaticls.com>
2019-03-07 15:53:18 +0100

    Add xml configuration

--- src/main/java/guru/springframework/springjokesapp/SpringJokesAppApplication.java
+++ src/main/java/guru/springframework/springjokesapp/SpringJokesAppApplication.java
@@ -2,8 +2,10 @@ package guru.springframework.springjokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ImportResource;
+/* when we are using xml config we need ImportResource*/
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class SpringJokesAppApplication {
 
     public static void main(String[] args) {
